0402a7dfcb74c82725e0cea8f5b8454fac3a58b1
William Harris <17HarriW@fulford.york.sch.uk>
2022-10-07 09:59:16 +0100

    Add project files.

---
+++ I.P.O/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I.P.O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius = Input();
+
+            // Process - Calculate volume of a sphere
+            double volume = Math.PI * (4/3) * (radius ^ 3);
+
+            // Output - Display volume
+            Console.WriteLine($"The volume is {volume}");
+
+            Console.ReadLine();
+
+        }
+        static int Input()
+        {
+            // Input - Retrieve radius from user
+            Console.Write("Enter an integer radius for the sphere: ");
+            string strRadius = Console.ReadLine();
+            int radius = int.Parse(strRadius);
+            return radius;
+        }
+    }
+}
